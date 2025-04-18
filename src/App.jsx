1c940e0d5f03cb28d92ef67d6e57c5b9df149a95
abu-abdullah22 <rorschachmayhem@gmail.com>
2025-04-18 17:47:38 +0600

    tailwind v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import './App.css'
-
 function App() {
 
+  
 
   return (
     <>
-      <h2 style={{border: '1px solid #4b5362', padding: '20px', margin: '10px', backgroundColor: '#31363f', borderRadius: '10px', minHeight: '100px'}}>Some title here</h2>
+     <h1 className="text-3xl font-bold underline">Hello</h1>
     </>
   )
 }
